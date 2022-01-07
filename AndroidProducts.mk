d2ea472c903df8d988e410f188aea6fcafad9cee
Eliminater74 <eliminater74@gmail.com>
2022-01-07 10:52:43 -0500

    sti6140d360: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 0 Manufacturer: askey Model: sti6140d360 Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_sti6140d360.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_sti6140d360-user \
+    twrp_sti6140d360-userdebug \
+    twrp_sti6140d360-eng
---
+++ twrp_sti6140d360.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from sti6140d360 device
+$(call inherit-product, device/askey/sti6140d360/device.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := sti6140d360
+PRODUCT_NAME := twrp_sti6140d360
+PRODUCT_BRAND := Onn
+PRODUCT_MODEL := sti6140d360
+PRODUCT_MANUFACTURER := askey
